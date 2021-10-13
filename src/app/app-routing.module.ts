c20fea2d92a264d15444a28456472e75f3dcec14
bhupender <bhupender.singh@infostride.com>
2021-10-14 04:28:21 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+import { NavComponent } from './nav/nav.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+
+  {
+    path:'',
+    component:LoginComponent
+  },
+  {
+    path:'nav',
+    component:NavComponent
+  },
+  
+  
+  {
+    path:'signup',
+    component:SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
